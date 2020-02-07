62453497d4464c16c41533d61842824058a594b5
Melina Cirverius <mc222ap@student.lnu.se>
2020-02-07 14:25:51 +0100

    Adding functionality to get links

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
+/**
+ * This is the starting point of the application.
+ *
+ * @author Melina Cirverius
+ * @version 1.0.0
+ */
 'use strict'
 
-// const { resolve } = require('path')
-// const { readJson, writeJson } = require('fs-extra')
-// const { getLinks } = require('./lib/scraper')
-const fetch = require('node-fetch')
-const { JSDOM } = require('jsdom')
-
-// const url = 'http://vhost3.lnu.se:20080/weekend'
-
-// ;(async () => {
-//   try {
-//     const linkPromises = await getLinks(url)
-//     const [domResults] = await Promise.all([dom])
-//   }
-//   catch (error) {
-//     console.log(error)
-//   }
-// })
+const { getLinks } = require('./lib/scraper')
 
 ;(async () => {
   try {
-    const response = await fetch('http://vhost3.lnu.se:20080/weekend')
-    const text = await response.text()
-    const dom = await new JSDOM(text)
-    console.log(dom.window.document.querySelectorAll('a[href^="http://"]'), element => element.href)
+    const url = 'http://vhost3.lnu.se:20080/weekend'
+
+    const linkPromise = getLinks(url)
+
+    const [links] = await Promise.all([linkPromise])
+
+    const linksSet = new Set([...links])
+
+    console.log(linksSet)
   } catch (error) {
     console.log(error)
   }
