b9d4437f619527be70027545d7936fd27d1dd63c
Melina Cirverius <mc222ap@student.lnu.se>
2020-02-07 12:56:38 +0100

    Adding functions to scraper and app

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log('Hello World')
+'use strict'
+
+// const { resolve } = require('path')
+// const { readJson, writeJson } = require('fs-extra')
+// const { getLinks } = require('./lib/scraper')
+const fetch = require('node-fetch')
+const { JSDOM } = require('jsdom')
+
+// const url = 'http://vhost3.lnu.se:20080/weekend'
+
+// ;(async () => {
+//   try {
+//     const linkPromises = await getLinks(url)
+//     const [domResults] = await Promise.all([dom])
+//   }
+//   catch (error) {
+//     console.log(error)
+//   }
+// })
+
+;(async () => {
+  try {
+    const response = await fetch('http://vhost3.lnu.se:20080/weekend')
+    const text = await response.text()
+    const dom = await new JSDOM(text)
+    console.log(dom.window.document.querySelectorAll('a[href^="http://"]'), element => element.href)
+  } catch (error) {
+    console.log(error)
+  }
+})()
