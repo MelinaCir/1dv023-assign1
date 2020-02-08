960067c6432885ab752e9fc337e6de51479e91ae
Melina Cirverius <mc222ap@student.lnu.se>
2020-02-08 14:19:15 +0100

    Adding calendar scraper

--- app.js
+++ app.js
@@ -7,19 +7,34 @@
 'use strict'
 
 const { getLinks } = require('./lib/scraper')
+const { getAvailableDate } = require('./lib/calendarScraper')
+// const { getAvailableMovie } = require('./lib/cinemaScraper')
+// const { getAvailableDinner } = require('./lib/dinnerScraper')
+
+const args = process.argv.slice(2)
+
+if (args.length === 0) {
+  console.error('ERROR: No argument(s).')
+  process.exit(0)
+}
 
 ;(async () => {
   try {
-    const url = 'http://vhost3.lnu.se:20080/weekend'
-
-    const linkPromise = getLinks(url)
+    const linkPromise = getLinks(args)
 
     const [links] = await Promise.all([linkPromise])
 
-    const linksSet = new Set([...links])
+    const linkArray = Array.from(new Set([...links]))
 
-    console.log(linksSet)
+    console.log('Scraping links...OK')
+
+    const date = getAvailableDate(linkArray[0])
+
+    // console.log('Recommendations')
+    // console.log('===============')
+    // console.log('* On ', day,' the movie "', movie,'" starts at ', movieTime,
+    // ' and there is a free table between ', dinnerTime, '.')
   } catch (error) {
-    console.log(error)
+    console.log('Error: ', error)
   }
 })()
